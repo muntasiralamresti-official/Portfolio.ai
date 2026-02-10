a80ccba0bfda5a296bb8900b0e638f2f83fd0866
muntasiralamresti-official <muntasiralamresti@gmail.com>
2026-02-10 20:31:53 +0600

    Nav Bar Updated

--- main.js
+++ main.js
@@ -221,6 +221,25 @@ projectCards.forEach(card => {
     });
 });
 
+// ================================
+// MOBILE MENU TOGGLE
+// ================================
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+// Close menu when clicking link
+document.querySelectorAll(".nav-items a").forEach(link => {
+  link.addEventListener("click", () => {
+    menuToggle.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+
 
 // ==========================================
 // CONSOLE MESSAGE
